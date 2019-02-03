e57e036d57487fc54831b832e4db76e3765c3da3
Chris Andrikanich <chris@andrikanich.com>
2019-02-03 15:22:39 -0500

    updated about me section - CA

--- src/components/Biography.vue
+++ src/components/Biography.vue
@@ -1,3 +1,22 @@
 <template>
-    <h1>My Story</h1>
+    <section>
+        <header>
+            <h1>Learn more about me...</h1>
+        </header>
+        <article>
+            <img src="i/cwa.jpg" class="img-rounded img-responsive" alt="Chris Andrikanich" />
+            <p>For over 18 years, I taught myself HTML and CSS to market Cleveland books by assisting great local authors with their social media and web presence.</p>
+
+            <p>That passion for learning how to code led me to Tech Elevator where I was able to expand my developer skillset and move on to the world of web development.</p>
+
+            <p>Currently, I am a Front-End Developer at Dealer Tire in Cleveland, working on the International Experience team supporting China and Canada.</p>
+
+            <p>When I am not running around with the kids, hanging with my wife, playing volleyball, or enjoying a locally brewed beer, I am most-likely reading up on HTML, CSS, and Javascript.</p>
+
+            <p>And yes, I am the Cleveland guy who's twitter account is the same name as Google's parent company: @alphabet.</p>
+            
+            <p>Shoot me a message, if you want a team player who is willing to learn, wants to be creative and try new things.</p>
+
+        </article>
+    </section>
 </template>
