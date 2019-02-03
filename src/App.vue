3e31a3159d66f87894f572af3af4e667fda3dc56
Chris Andrikanich <chris@andrikanich.com>
2019-02-03 16:59:25 -0500

    cleaning up the navigation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About Me</router-link> |
-      <router-link to="/resume">My Resume</router-link> |
-      <router-link to="/alphabet">The Alphabet Story</router-link> |
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About Me</router-link>
+      <router-link to="/resume">My Resume</router-link>
+      <router-link to="/alphabet">The Alphabet Story</router-link>
       <router-link to="/projects">My Projects</router-link>
     </div>
     <router-view/>
@@ -19,7 +19,7 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  // text-align: center;
   color: #2c3e50;
 }
 #nav {
@@ -27,11 +27,20 @@
   background-color: #ccc;
 
   a {
-    font-weight: bold;
     color: #2c3e50;
+    text-decoration: none;
+    border-right: 1px solid green;
+    margin-right: 15px;
+    padding-right: 15px;
     &.router-link-exact-active {
       color: #42b983;
+      font-weight: bold;
+      text-decoration-color: #2c3e50;
+    }
+    &:last-child {
+      border-right: none;
     }
   }
+
 }
 </style>
