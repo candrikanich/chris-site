67c3b52bb476ed251e55da32c0526e2fdcee3345
Chris Andrikanich <chris@andrikanich.com>
2019-02-03 16:26:00 -0500

    create the basic landing page

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,3 +1,56 @@
 <template>
-    <h1>Chris Andrikanich</h1>
+    <section>
+        <header>
+            <h1>Hi, I'm Chris.</h1>
+            <h2>Nice to meet you. </h2>
+        </header>
+        <article>
+            [ put some buttons here ]
+        </article>
+    </section>
+    
 </template>
+
+<style lang="less">
+    body {
+        background: url("../assets/img/cwa.jpg") center center no-repeat;
+        background-attachment: fixed;
+        background-size: cover;
+        background-color: #464646;
+    }
+    section {
+        // order: 1px solid #fff;
+        
+        header {
+            // border: 1px solid #990000;
+            margin-top: 68%;
+            background-color: #ccc;
+
+            h1 {
+                font-size: 4.5em;
+                font-weight: bold;
+                opacity: 1;
+            }
+
+            h2 {
+                font-size: 2.6em;
+                font-weight: normal;
+            }
+
+            h1, h2 {
+                padding: 0;
+                margin: 0;
+                line-height: 1.1em;
+                text-shadow: 0 0 3px #464646;
+            }
+        }
+        article {
+            // border: 1px solid yellow;
+            margin-top: 30%;
+            background-color: #464646;
+            opacity: 0.5;
+        }
+
+    }
+</style>
+
