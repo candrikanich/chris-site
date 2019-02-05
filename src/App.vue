38c020d1de294f2051f1ae6ec1cd33bdc8dc851f
Chris Andrikanich <chris@andrikanich.com>
2019-02-04 23:19:51 -0500

    get the landing page working

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,58 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <div id="app" class="grid-container">
+
+    <!-- NAVIGATION -->
+    <nav>
       <router-link to="/">Home</router-link>
       <router-link to="/about">About Me</router-link>
       <router-link to="/resume">My Resume</router-link>
       <router-link to="/alphabet">The Alphabet Story</router-link>
       <router-link to="/projects">My Projects</router-link>
-    </div>
+    </nav>
+
+    <!-- CONTENT INJECTION -->
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
-@import url("assets/styles/main.less");
+  @import url("assets/styles/main.less");
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  background-color: #ccc;
+  html {
+    height: 100%;
 
-  a {
-    color: #2c3e50;
-    text-decoration: none;
-    border-right: 1px solid green;
-    margin-right: 15px;
-    padding-right: 15px;
-    &.router-link-exact-active {
-      color: #42b983;
-      font-weight: bold;
-      text-decoration-color: #2c3e50;
-    }
-    &:last-child {
-      border-right: none;
+    body {
+      min-height: 100%;
     }
   }
 
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+  nav {
+    padding: 30px;
+    background-color: #ccc;
+
+    a {
+      color: #2c3e50;
+      text-decoration: none;
+      border-right: 1px solid green;
+      margin-right: 15px;
+      padding-right: 15px;
+
+      &.router-link-exact-active {
+        color: #42b983;
+        font-weight: bold;
+        text-decoration-color: #2c3e50;
+      }
+
+      &:last-child {
+        border-right: none;
+      }
+    }
+
+  }
 </style>
--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,56 +1,65 @@
 <template>
-    <section>
+    <section class="landingPage">
         <header>
             <h1>Hi, I'm Chris.</h1>
             <h2>Nice to meet you. </h2>
         </header>
-        <article>
-            [ put some buttons here ]
-        </article>
     </section>
     
 </template>
 
-<style lang="less" scoped>
+<script>
+    export default {
+        name: 'landing-page',
+        data() {
+            return {
+                pageName: 'landingPage'
+            }
+        }
+    }
+</script>
+
+<style lang="less">
     @import url("../assets/styles/main.less");
 
-    section {
-        border: 1px solid red;
+    nav {
+        display: none;
+    }
+
+    section.landingPage {
         background: url("../assets/img/cwa.png") center center no-repeat;
         background-attachment: fixed;
         background-size: cover;
         background-color: @background-color;
-        height: 100%;
+        height: 100vh;
+        width: 100vw;
 
-        // header {
-        //     // border: 1px solid #990000;
-        //     margin-top: 68%;
-        //     background-color: #ccc;
+        header {
+            padding: 1em;
 
-        //     h1 {
-        //         font-size: 4.5em;
-        //         font-weight: bold;
-        //         opacity: 1;
-        //     }
+            h1 {
+                font-size: 3.0em;
+                font-weight: bold;
+            }
 
-        //     h2 {
-        //         font-size: 2.6em;
-        //         font-weight: normal;
-        //     }
+            h2 {
+                font-size: 1.8em;
+                font-weight: normal;
+            }
 
-        //     h1, h2 {
-        //         padding: 0;
-        //         margin: 0;
-        //         line-height: 1.1em;
-        //         text-shadow: 0 0 3px #464646;
-        //     }
-        // }
-        // article {
-        //     // border: 1px solid yellow;
-        //     margin-top: 30%;
-        //     background-color: #464646;
-        //     opacity: 0.5;
-        // }
+            h1, h2 {
+                padding: 0;
+                margin: 0;
+                line-height: 1.0em;
+                text-shadow: 0 0 3px #464646;
+                color: #fff;
+            }
+        }
+        article {
+            padding-top: 30%;
+            background-color: #464646;
+            opacity: 0.5;
+        }
 
     }
 </style>
