76037b6d338309e31d657b2198e1f48007711c9d
Chris Andrikanich <chris@andrikanich.com>
2019-02-11 21:33:47 -0500

    project site update

--- src/components/ProjectStatus.vue
+++ src/components/ProjectStatus.vue
@@ -6,7 +6,7 @@
         <article>
             <p>Here is a list of the technologies used to create this web site.</p>
             <ul>
-                <li>Built using Vue.js <i class="fab fa-vuejs"></i> and HTML <i class="fab fa-html"></i></li>
+                <li>Built using Vue.js <i class="fab fa-vuejs"></i> and HTML <i class="fab fa-html5"></i></li>
                 <li>Styled using CSS <i class="fab fa-css3"></i> and LESS <i class="fab fa-less"></i></li>
                 <li>Hosted by Amazon Web Services S3 <i class="fab fa-aws"></i></li> 
                 <li>Code repository stored on Github <i class="fab fa-github"></i></li>
